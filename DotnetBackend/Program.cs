112ff2a3a4801f07f59501ae1b9a212ee587df6c
JonasEHartig <jonashartige@gmail.com>
2025-01-29 10:40:19 +0100

    Database start

--- DotnetBackend/Program.cs
+++ DotnetBackend/Program.cs
@@ -1,38 +1,24 @@
-using MongoDB.Driver;
 using MySql.Data.MySqlClient;
 using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// Get connection string from appsettings.json
+var connectionString = builder.Configuration.GetConnectionString("MySQL");
+
+// Register MySqlConnection as a service
+builder.Services.AddTransient<MySqlConnection>(_ => new MySqlConnection(connectionString));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
 app.UseHttpsRedirection();
-try
-        {
-            var connectionString = builder.Configuration.GetConnectionString("MySQL");
 
-            using (var connection = new MySqlConnection(connectionString))
-            {
-                connection.Open();
-                Console.WriteLine("Connected to MySQL server successfully!");
-            }
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Failed to connect to MySQL server: {ex.Message}");
-        }
-        
+MySqlConnection connection = new MySqlConnection(connectionString);
+
+connection.Open();
+
+DatabaseHelper databasehelper = new DatabaseHelper(connection);
+
+var a = databasehelper.GetVariable("rooms","name");
+
 app.Run();
-
-
-
-
